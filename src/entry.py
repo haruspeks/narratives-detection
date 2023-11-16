c6e9d2ca730a8b2263c3c24f662d517199d5e992
Fraser Crichton <52700324+frasercrichton@users.noreply.github.com>
2023-11-16 14:08:37 +0100

    feat: databaseconnection.

--- src/entry.py
+++ src/entry.py
@@ -3,15 +3,19 @@ import sys
 from flask import Flask, request
 from matchMaker import MatchMaker 
 from entityExtractor import EntityExtractor 
+from data import Data 
 
 app = Flask(__name__)
 
+data = Data()
 matchMaker = MatchMaker()
 entityExtractor = EntityExtractor()
 
 @app.route('/', methods = ['POST'])
 def default():
    print('post called')
+   data.get('id')
+
    text = request.form['entities']
    entities = entityExtractor.extract(text)
 
